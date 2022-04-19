0c3e1c64e1d854b1f7dbb7c43d2cd53218107eb0
ali <alioliaee31@gmail.com>
2022-04-19 19:02:17 +0430

    :zap: add auth middleware

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -42,9 +42,13 @@ export const removePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
   const { id } = req.params;
+  if (req.userId) return res.json({ message: "unauthenticated" });
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("no post with this id");
   const post = await PostMessage.findById(id);
+  const likeIndex = post.likes.findIndex((id) => id === String(req.userId));
+  if (likeIndex !== -1) post.likes.push(req.userId);
+  else post.likes = post.likes.filter((id) => id !== String(req.userId));
   const updatePost = await PostMessage.findByIdAndUpdate(id, {
     likeCount: post.likeCount + 1,
   });
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,13 +6,14 @@ import {
   removePost,
   likePost,
 } from "../controllers/posts.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/", getPosts);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", removePost);
-router.patch("/like-post/:id", likePost);
+router.get("/", auth, getPosts);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, removePost);
+router.patch("/like-post/:id", auth, likePost);
 
 export default router;
