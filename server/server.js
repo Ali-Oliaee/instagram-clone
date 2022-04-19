88adb267e81e2a1bc16fb428bc18808f82bc6b46
ali <alioliaee31@gmail.com>
2022-04-19 12:19:11 +0430

    :zap: setup database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+const port = process.env.PORT ?? 5000;
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() =>
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+  )
+  .catch((err) => console.log("err", err));
