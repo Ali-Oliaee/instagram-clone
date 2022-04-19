0c94fc832cb1cb2a65575b87129fcaac6881869e
ali <alioliaee31@gmail.com>
2022-04-19 18:31:59 +0430

    :zap: set users endpoint

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,10 +3,46 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
-export const signup = async (req, res) => {
-  res.send("signup");
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) return res.json({ message: "user doesnt exist" });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect)
+      return res.json({ message: "password is not correct" });
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      "test",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json("something is wrong");
+  }
 };
 
-export const signin = async (req, res) => {
-  res.send("singin");
+export const signup = async (req, res) => {
+  const { username, email, password, image } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json("user already exist");
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      image,
+    });
+    const token = jwt.sign({ email: newUser.email, id: newUser._id }, "test", {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ newUser, token });
+  } catch (error) {
+    res.status(500).send("something is wrong");
+  }
 };
--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  image: { type: String, required: true },
+  image: { type: String },
   id: { type: String },
 });
 
