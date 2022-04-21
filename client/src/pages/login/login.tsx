4159f06f8823477922f4e4df5151ba7095ab8e98
ali <alioliaee31@gmail.com>
2022-04-21 11:35:11 +0430

    :zap: add forms and validations

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,10 +1,56 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
+import { useTranslation } from 'react-i18next'
+import { Button, Form, Input } from 'antd'
 import './style.scss'
 
+interface User {
+  email: String,
+  password: String,
+}
+
 function LoginPage() {
+  const { t } = useTranslation()
+  const handleSubmit = ({ email, password } : User) => {
+    console.log('user')
+  }
   return (
     <div className="login-page">
-      Login page
+      <div className="form-container">
+        <img src="../../assets/images/logo.svg" alt="logo" width={220} height={50} />
+        <h4>{t('login-description')}</h4>
+        <Form onFinish={handleSubmit}>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: t('require-email'),
+              },
+              {
+                type: 'email',
+                message: t('invalid-email'),
+              },
+            ]}
+          >
+            <Input placeholder={t('Email')} type="email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: t('require-password'),
+
+              },
+            ]}
+          >
+            <Input placeholder={t('Password')} type="password" />
+          </Form.Item>
+          <Button htmlType="submit" type="primary" block>submit</Button>
+        </Form>
+        <h4>{t('dont-have-account')}</h4>
+        <a href="/">{t('login')}</a>
+      </div>
     </div>
   )
 }
--- client/src/pages/signup/signup.tsx
+++ client/src/pages/signup/signup.tsx
@@ -1,18 +1,79 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import { useTranslation } from 'react-i18next'
-import { Button } from 'antd'
+import { Button, Form, Input } from 'antd'
 import './style.scss'
+
+interface NewUser {
+  username: String,
+  email: String,
+  password: String,
+  confirmPassword: String
+}
 
 function SignupPage() {
   const { t } = useTranslation()
-  const handleSubmit = (values : any) => {
-    console.log('user', values)
+  const handleSubmit = ({
+    username, email, password, confirmPassword,
+  } : NewUser) => {
+    console.log('user')
   }
   return (
     <div className="signup-page">
       <div className="form-container">
         <img src="../../assets/images/logo.svg" alt="logo" width={220} height={50} />
-        <Button type="primary">submit</Button>
+        <h4>{t('signup-description')}</h4>
+        <Form onFinish={handleSubmit}>
+          <Form.Item
+            name="userName"
+            rules={[
+              {
+                required: true,
+                message: t('require-username'),
+              },
+            ]}
+          >
+            <Input placeholder={t('Username')} />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: t('require-email'),
+              },
+              {
+                type: 'email',
+                message: t('invalid-email'),
+              },
+            ]}
+          >
+            <Input placeholder={t('Email')} type="email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: t('require-password'),
+
+              },
+            ]}
+          >
+            <Input placeholder={t('Password')} type="password" />
+          </Form.Item>
+          <Form.Item
+            name="confirmPassword"
+            rules={[{
+              required: true,
+              message: t('require-confirm-password'),
+            }]}
+          >
+            <Input placeholder={t('Confirm Password')} type="password" />
+          </Form.Item>
+          <Button htmlType="submit" type="primary" block>submit</Button>
+        </Form>
+        <h4>{t('already-have-account')}</h4>
+        <a href="/">{t('signin')}</a>
       </div>
     </div>
   )
