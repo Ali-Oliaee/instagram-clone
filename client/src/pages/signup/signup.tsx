839369cf132d828d1d24df39a78fd4fcac4b53b9
ali <alioliaee31@gmail.com>
2022-04-21 11:19:14 +0430

    :zap: add antd

--- client/src/pages/signup/signup.tsx
+++ client/src/pages/signup/signup.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
-import { Typography } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
+import { Button } from 'antd'
 import './style.scss'
 
 function SignupPage() {
   const { t } = useTranslation()
+  const handleSubmit = (values : any) => {
+    console.log('user', values)
+  }
   return (
     <div className="signup-page">
       <div className="form-container">
         <img src="../../assets/images/logo.svg" alt="logo" width={220} height={50} />
-        <Typography variant="h4">{t('signup-description')}</Typography>
+        <Button type="primary">submit</Button>
       </div>
     </div>
   )
