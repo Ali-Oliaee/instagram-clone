65dfe0b0c2fdb8aa9ba0bc28c61bbe3fa27418fb
ali <alioliaee31@gmail.com>
2022-04-22 10:47:47 +0430

    :sparkles: add logo

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import { useTranslation } from 'react-i18next'
 import { Button, Form, Input } from 'antd'
+import { Logo } from '../../components'
 import './style.scss'
 
 interface User {
@@ -15,49 +16,51 @@ function LoginPage() {
   }
   return (
     <div className="login-page">
-      <div className="form-container">
-        <img src="../../assets/images/logo.svg" alt="logo" width={220} height={50} />
-        <h4>{t('login-description')}</h4>
-        <Form onFinish={handleSubmit}>
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: t('require-email'),
-              },
-              {
-                type: 'email',
-                message: t('invalid-email'),
-              },
-            ]}
-          >
-            <Input placeholder={t('Email')} type="email" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: t('require-password'),
-              },
-              {
-                min: 6,
-                message: t('min-password'),
-              },
-            ]}
-          >
-            <Input placeholder={t('Password')} type="password" />
-          </Form.Item>
-          <Button htmlType="submit" type="primary" block>submit</Button>
-        </Form>
-        <h4>
+      <section>
+        <div className="form-container">
+          <Logo />
+          <h4>{t('login-description')}</h4>
+          <Form onFinish={handleSubmit}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: t('require-email'),
+                },
+                {
+                  type: 'email',
+                  message: t('invalid-email'),
+                },
+              ]}
+            >
+              <Input size="middle" placeholder={t('Email')} type="email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: t('require-password'),
+                },
+                {
+                  min: 6,
+                  message: t('min-password'),
+                },
+              ]}
+            >
+              <Input size="middle" placeholder={t('Password')} type="password" />
+            </Form.Item>
+            <Button htmlType="submit" type="primary" block>submit</Button>
+            <h4>{t('privacy-rules')}</h4>
+          </Form>
+        </div>
+        <h4 className="signup-link">
           {t('dont-have-account')}
-          {' '}
-          <a href="/">{t('login')}</a>
+          {'  '}
+          <a href="/">{t('signup')}</a>
         </h4>
-
-      </div>
+      </section>
     </div>
   )
 }
--- client/src/pages/signup/signup.tsx
+++ client/src/pages/signup/signup.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import { useTranslation } from 'react-i18next'
 import { Button, Form, Input } from 'antd'
+import { Logo } from '../../components'
 import './style.scss'
 
 interface NewUser {
@@ -19,79 +20,80 @@ function SignupPage() {
   }
   return (
     <div className="signup-page">
-      <div className="form-container">
-        <img src="../../assets/images/logo.svg" alt="logo" width={220} height={50} />
-        <h4>{t('signup-description')}</h4>
-        <Form onFinish={handleSubmit}>
-          <Form.Item
-            name="userName"
-            rules={[
-              {
+      <section>
+        <div className="form-container">
+          <Logo />
+          <h4>{t('signup-description')}</h4>
+          <Form onFinish={handleSubmit}>
+            <Form.Item
+              name="userName"
+              rules={[
+                {
+                  required: true,
+                  message: t('require-username'),
+                },
+              ]}
+            >
+              <Input size="middle" placeholder={t('Username')} />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: t('require-email'),
+                },
+                {
+                  type: 'email',
+                  message: t('invalid-email'),
+                },
+              ]}
+            >
+              <Input size="middle" placeholder={t('Email')} type="email" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: t('require-password'),
+                },
+                {
+                  min: 6,
+                  message: t('min-password'),
+                },
+              ]}
+            >
+              <Input size="middle" placeholder={t('Password')} type="password" />
+            </Form.Item>
+            <Form.Item
+              name="confirmPassword"
+              dependencies={['password']}
+              rules={[{
                 required: true,
-                message: t('require-username'),
-              },
-            ]}
-          >
-            <Input placeholder={t('Username')} />
-          </Form.Item>
-          <Form.Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: t('require-email'),
-              },
-              {
-                type: 'email',
-                message: t('invalid-email'),
-              },
-            ]}
-          >
-            <Input placeholder={t('Email')} type="email" />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: t('require-password'),
-              },
-              {
-                min: 6,
-                message: t('min-password'),
-              },
-            ]}
-          >
-            <Input placeholder={t('Password')} type="password" />
-          </Form.Item>
-          <Form.Item
-            name="confirmPassword"
-            dependencies={['password']}
-            rules={[{
-              required: true,
-              message: t('require-confirm-password'),
+                message: t('require-confirm-password'),
 
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve()
-                }
-                return Promise.reject(new Error(t('passwords-not-match')))
               },
-            })]}
-          >
-            <Input placeholder={t('Confirm Password')} type="password" />
-          </Form.Item>
-          <Button htmlType="submit" type="primary" block>submit</Button>
-        </Form>
-        <h4>
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error(t('passwords-not-match')))
+                },
+              })]}
+            >
+              <Input size="middle" placeholder={t('Confirm Password')} type="password" />
+            </Form.Item>
+            <Button htmlType="submit" type="primary" block>submit</Button>
+          </Form>
+        </div>
+        <h4 className="signin-link">
           {t('already-have-account')}
           {' '}
           <a href="/">{t('signin')}</a>
         </h4>
-
-      </div>
+      </section>
     </div>
   )
 }
