00a56352728afea3bd6be9cb090682954397a1c0
ali <alioliaee31@gmail.com>
2022-04-22 21:49:48 +0430

    :lipstick: add 404 page

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import { Route, Routes } from 'react-router-dom'
-import { LoginPage, SignupPage } from '../pages'
+import { LoginPage, NotFoundPage, SignupPage } from '../pages'
 
 function MainRouter() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
 }
