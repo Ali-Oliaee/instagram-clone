2e69b1b14cb5e7a782a5abf7d16c21017bf49cf0
ali <alioliaee31@gmail.com>
2022-04-25 15:49:13 +0430

    :zap: add form validation

--- client/src/components/add-post-modal/add-post-modal.tsx
+++ client/src/components/add-post-modal/add-post-modal.tsx
@@ -16,13 +16,29 @@ function AddPostModal({ visible }:any) {
   return (
     <Modal visible={visible} closable onCancel={() => setSearchParams('')} title="Add Post" footer={null}>
       <Form onFinish={postFile}>
-        <Form.Item name="title">
+        <Form.Item
+          name="title"
+          rules={[
+            {
+              required: true,
+              message: t('requred-title'),
+            },
+          ]}
+        >
           <Input placeholder={t('title')} />
         </Form.Item>
         <Form.Item name="content">
           <Input.TextArea placeholder={t('description')} />
         </Form.Item>
-        <Form.Item name="post">
+        <Form.Item
+          name="post"
+          rules={[
+            {
+              required: true,
+              message: t('requred-post'),
+            },
+          ]}
+        >
           <Upload>
             <Button icon={<UploadOutlined />}>{t('upload')}</Button>
             <span>{t('upload-text')}</span>
--- client/src/components/post-card/post-card.tsx
+++ client/src/components/post-card/post-card.tsx
@@ -1,10 +1,18 @@
 /* eslint-disable global-require */
 /* eslint-disable prefer-arrow/prefer-arrow-functions */
 import {
-  DeleteOutlined, HeartFilled, MoreOutlined, HeartOutlined,
+  DeleteOutlined,
+  HeartFilled,
+  MoreOutlined,
+  HeartOutlined,
+  EnvironmentOutlined,
+  EnvironmentFilled,
+  MessageOutlined,
+  EditOutlined,
+  QuestionCircleOutlined,
 } from '@ant-design/icons'
 import {
-  Avatar, Button, Dropdown, Menu, Tag,
+  Avatar, Button, Dropdown, Menu, Popconfirm, Tag,
 } from 'antd'
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -25,8 +33,10 @@ function PostCard({
   title, description, creator, tags, likes, image, createdAt, id,
 }: Post) {
   const [like, setLike] = useState(false)
+  const [archive, setArchive] = useState(false)
   const { t } = useTranslation()
   const likePost = () => (like ? setLike(false) : setLike(true))
+  const archivePost = () => (archive ? setArchive(false) : setArchive(true))
 
   return (
     <div className="post-card">
@@ -39,7 +49,16 @@ function PostCard({
           trigger={['click']}
           overlay={(
             <Menu>
-              <Menu.Item danger icon={<DeleteOutlined />}>{t('delete')}</Menu.Item>
+              <Menu.Item icon={<EditOutlined />}>{t('edit')}</Menu.Item>
+              <Popconfirm
+                title={t('delete-confirm')}
+                onConfirm={() => console.log('ok')}
+                okText={t('yes')}
+                cancelText={t('no')}
+                icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+              >
+                <Menu.Item danger icon={<DeleteOutlined />}>{t('delete')}</Menu.Item>
+              </Popconfirm>
             </Menu>
                 )}
         >
@@ -48,19 +67,32 @@ function PostCard({
       </div>
       <img src={image} alt="post" />
       <div className="post-info">
-        <h2>{title}</h2>
-        <p>
-          {creator}
-          {description}
-        </p>
+        <div className="card-operations">
+          <h3>
+            <Button size="large" icon={like ? <HeartFilled style={{ color: 'red' }} /> : <HeartOutlined />} onClick={likePost} className="like-button" />
+            {likes.length}
+          </h3>
+          <span>
+            <Button size="large" icon={<MessageOutlined />} className="comment-button" />
+            <Button size="large" icon={archive ? <EnvironmentFilled /> : <EnvironmentOutlined />} onClick={archivePost} className="archive-button" />
+          </span>
+        </div>
+        <h2 className="title">{title}</h2>
+        <div className="description-container">
+          <span className="creator">
+            {creator}
+            :
+            {' '}
+          </span>
+          <span className="description">
+            {description}
+            <Button type="link" className="more-button">more...</Button>
+          </span>
+        </div>
         <div className="tags">
           {tags.length && tags.map((tag: string) => <Tag key={tag} className="tag">{tag}</Tag>)}
         </div>
-        <p>{createdAt}</p>
-        <h3>
-          <Button icon={like ? <HeartFilled style={{ color: 'red' }} /> : <HeartOutlined />} onClick={likePost} className="like-button" />
-          {` ${likes.length}likes`}
-        </h3>
+        <span className="date">{createdAt}</span>
       </div>
     </div>
   )
