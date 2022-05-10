a670fde59ee7cbeab6f3f232699f22032aae8d85
ali <alioliaee31@gmail.com>
2022-05-10 21:54:59 +0430

    add desktop wrapper

--- src/components/posts-wrapper/posts-wrapper.tsx
+++ src/components/posts-wrapper/posts-wrapper.tsx
@@ -1,5 +1,7 @@
 import { PostCard } from '../post-card'
+import { useMediaQuery } from 'usehooks-ts'
 import './style.scss'
+import { Image } from 'antd'
 
 interface Post {
   title: string
@@ -17,9 +19,11 @@ interface PostProp {
 }
 
 function PostsWrapper({ posts }: PostProp) {
+  const isMobile = useMediaQuery('(max-width: 500px)')
+
   return (
-    <div className="posts-wrapper">
-      {posts.map((post : Post) => (
+    <div className={isMobile ? "posts-wrapper-mobile" : "posts-wrapper-desktop"}>
+      {isMobile ? posts.map((post : Post) => (
         <PostCard
           key={post.id}
           title={post.title}
@@ -30,8 +34,18 @@ function PostsWrapper({ posts }: PostProp) {
           image={post.image}
           createdAt={post.createdAt ?? Date()}
           id={post.id}
-        />
-      ))}
+        />  
+      )
+      ) : posts.map((post : Post) => (
+            <Image
+              key={post.id}
+              src={post.image}
+              alt={post.title}
+              width={200}
+              height={200}
+              />
+          ))
+      }
     </div>
   )
 }
