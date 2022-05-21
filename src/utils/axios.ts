405d63c884357f702d37db0a7a6ab44c605138c8
ali <alioliaee31@gmail.com>
2022-05-21 21:48:58 +0430

    fix routes

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,9 +3,15 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'http://127.0.0.1:8000/',
   headers: {
-    authorization: `Bearer ${JSON.parse(localStorage.getItem('user') || '{}').tokens.access}`,
     'Content-Type': 'application/json',
   },
+})
+
+instance.interceptors.request.use((config : any) => {
+  const token = JSON.parse(localStorage.getItem('user') ?? '{}')?.tokens?.access
+  // eslint-disable-next-line no-param-reassign
+  config.headers.Authorization = token && `Bearer ${token}`
+  return config
 })
 
 export default instance
