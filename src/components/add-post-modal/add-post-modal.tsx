66d9623d0d4d8c0a8e4c0d528ca0159a8780fce7
ali <alioliaee31@gmail.com>
2022-05-21 23:56:07 +0430

    set add post api

--- src/components/add-post-modal/add-post-modal.tsx
+++ src/components/add-post-modal/add-post-modal.tsx
@@ -5,14 +5,39 @@ import {
 import { useTranslation } from 'react-i18next'
 import { useSearchParams } from 'react-router-dom'
 import qs from 'query-string'
+import axios from '../../utils/axios'
 import { FloatLabel } from '../float-label'
 import './style.scss'
 
-function AddPostModal({ post, onFinish }:any) {
+function AddPostModal({ post }:any) {
   const [searchParams, setSearchParams] = useSearchParams()
   const { t } = useTranslation()
   const [form] = Form.useForm()
   const QS = qs.parse(window.location.search)
+
+  const addPost = ({
+    post: image, title, caption, tags,
+  } : any) => {
+    const postImage = image[0].originFileObj
+    const formData = new FormData()
+    formData.append('file', postImage)
+    formData.append('title', title)
+    formData.append('caption', caption)
+
+    axios.post(
+      '/posts/create/',
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    )
+  }
+
+  const editPost = (values : any) => {
+    console.log('values edit', values)
+  }
 
   return (
     <Modal
@@ -23,7 +48,7 @@ function AddPostModal({ post, onFinish }:any) {
       footer={null}
       className="add-post-modal"
     >
-      <Form onFinish={onFinish} form={form}>
+      <Form onFinish={post ? editPost : addPost} form={form}>
         {!!QS.add && (
         <Form.Item
           name="post"
@@ -33,6 +58,8 @@ function AddPostModal({ post, onFinish }:any) {
               message: t('requred-post'),
             },
           ]}
+          valuePropName="fileList"
+          getValueFromEvent={(e) => (Array.isArray(e) ? e : e && e.fileList)}
         >
           <Upload>
             <Button block icon={<UploadOutlined />}>{t('upload')}</Button>
@@ -51,8 +78,8 @@ function AddPostModal({ post, onFinish }:any) {
         >
           <FloatLabel label={t('title')} autoFocus value={form.getFieldValue('title')} />
         </Form.Item>
-        <Form.Item name="content">
-          <FloatLabel textarea label={t('description')} value={form.getFieldValue('content')} />
+        <Form.Item name="caption">
+          <FloatLabel textarea label={t('description')} value={form.getFieldValue('caption')} />
         </Form.Item>
         <Form.Item name="tags">
           <Select mode="tags" placeholder={t('tags')} />
