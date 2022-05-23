f999d395a4da7987a380193634440ee5b55004ee
ali <alioliaee31@gmail.com>
2022-05-24 01:32:04 +0430

    add axios options

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,9 +2,14 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: 'http://127.0.0.1:8000/',
+  timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
 })
 
-instance.interceptors.request.use((config : any) => {
+instance.interceptors.request.use((config: any) => {
   const token = JSON.parse(localStorage.getItem('user') ?? '{}')?.tokens?.access
   // eslint-disable-next-line no-param-reassign
   config.headers.Authorization = token && `Bearer ${token}`
