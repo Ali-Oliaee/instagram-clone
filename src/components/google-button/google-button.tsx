b8199a5c7c6d566ba85a94761f28d8bca5e0c924
ali <alioliaee31@gmail.com>
2022-05-26 18:12:18 +0430

    handle onsubmit

--- src/components/google-button/google-button.tsx
+++ src/components/google-button/google-button.tsx
@@ -1,17 +1,34 @@
-import { Button } from 'antd'
+import { useState } from 'react'
+import { Button, message } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { GoogleLogin } from 'react-google-login'
 import { GoogleOutlined } from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom'
+import axios from '../../utils/axios'
 import './style.scss'
 
 function GoogleButton() {
   const { t } = useTranslation()
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
   const onSuccess = (res: any) => {
-    console.log('res', res)
+    setLoading(true)
+    axios.post('users/login/', {
+      googleId: res.googleId,
+    })
+      .then(({ data }) => {
+        message.success(data.message)
+        localStorage.setItem('user', JSON.stringify(data))
+        navigate('/new-user')
+      })
+      .catch(({ response }) => message.error(response.data.message ?? response.data.email))
+      .finally(() => setLoading(false))
   }
+
   const onFailure = (err: any) => {
     console.log('err', err)
   }
+
   return (
     <GoogleLogin
       clientId="1028978384739-u2f7gmkkcilpt78sl9qh4lhnnpk4hq4p.apps.googleusercontent.com"
