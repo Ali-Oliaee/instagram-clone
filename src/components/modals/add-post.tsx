94fb957aa16106ba87164681d69a4a415e3f8101
ali <alioliaee31@gmail.com>
2022-06-09 23:04:40 +0430

    optimize add modal

--- src/components/modals/add-post.tsx
+++ src/components/modals/add-post.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 import { useState } from 'react'
-import { UploadOutlined } from '@ant-design/icons'
+import { InboxOutlined, UploadOutlined } from '@ant-design/icons'
 import {
   Button, Form, message, Modal, Select, Switch, Upload,
 } from 'antd'
@@ -7,6 +8,7 @@ import { useTranslation } from 'react-i18next'
 import { useSearchParams } from 'react-router-dom'
 import qs from 'query-string'
 import { useQueryClient } from 'react-query'
+import Dragger from 'antd/lib/upload/Dragger'
 import axios from '../../utils/axios'
 import { FloatLabel } from '../float-label'
 import './style.scss'
@@ -18,12 +20,14 @@ function AddPostModal() {
   const [form] = Form.useForm()
   const QS = qs.parse(window.location.search)
   const queryClient = useQueryClient()
+  const [secondModalVisible, setSecondModalVisible] = useState(false)
 
   const addPost = ({
     post: image, title, caption, tags, enableComments,
   } : any) => {
     setLoading(true)
-    const postImage = image[0].originFileObj
+    const { file } = form.getFieldValue('post')
+    const postImage = file.originFileObj
     const formData = new FormData()
     formData.append('file', postImage)
     formData.append('title', title)
@@ -38,7 +42,7 @@ function AddPostModal() {
         },
       },
     ).then(() => {
-      setSearchParams({})
+      setSecondModalVisible(false)
       queryClient.invalidateQueries('posts')
       form.resetFields()
       message.success('post added successfully!')
@@ -46,55 +50,87 @@ function AddPostModal() {
   }
 
   return (
-    <Modal
-      visible={!!QS.add}
-      closable
-      onCancel={() => setSearchParams({})}
-      title={t('add-post')}
-      footer={null}
-      className="add-post-modal"
-      destroyOnClose
-    >
-      <Form onFinish={addPost} form={form}>
-        <Form.Item
-          name="post"
-          rules={[
-            {
-              required: true,
-              message: t('required-post'),
-            },
-          ]}
-        >
-          <Upload>
-            <Button block icon={<UploadOutlined />}>{t('upload')}</Button>
-            <span className="upload-description">{t('upload-description')}</span>
-          </Upload>
-        </Form.Item>
-        <Form.Item
-          name="title"
-          rules={[
-            {
-              required: true,
-              message: t('requred-title'),
-            },
-          ]}
-        >
-          <FloatLabel label={t('title')} autoFocus value={form.getFieldValue('title')} />
-        </Form.Item>
-        <Form.Item name="caption">
-          <FloatLabel textarea label={t('description')} value={form.getFieldValue('caption')} />
-        </Form.Item>
-        <Form.Item name="tags">
-          <Select open={false} mode="tags" placeholder={t('tags')} />
-        </Form.Item>
-        <Form.Item label={t('allow-comments')} initialValue valuePropName="checked" name="enableComments">
-          <Switch className="switch" />
-        </Form.Item>
-        <Button block type="primary" htmlType="submit" loading={loading}>
-          {t('submit')}
-        </Button>
-      </Form>
-    </Modal>
+    <>
+      <Modal
+        visible={!!QS.add}
+        closable
+        centered
+        onCancel={() => setSearchParams({})}
+        title={t('add-post')}
+        footer={null}
+        className="add-post-modal"
+        destroyOnClose
+      >
+        <Form form={form}>
+          <Form.Item
+            name="post"
+            rules={[
+              {
+                required: true,
+                message: t('required-post'),
+              },
+            ]}
+          >
+            <Dragger
+              name="file"
+              maxCount={1}
+              onChange={({ event }: any) => {
+                if (event) {
+                  message.success('file uploaded successfully.')
+                  setSearchParams({})
+                  setSecondModalVisible(true)
+                }
+              }}
+            >
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">Click or drag file to this area to upload</p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                band files
+              </p>
+            </Dragger>
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Modal
+        visible={secondModalVisible}
+        closable
+        centered
+        onCancel={() => setSecondModalVisible(false)}
+        title={t('add-post')}
+        footer={null}
+        className="add-post-modal"
+        destroyOnClose
+      >
+        <Form onFinish={addPost} form={form}>
+          <Form.Item
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: t('required-title'),
+              },
+            ]}
+          >
+            <FloatLabel label={t('title')} autoFocus value={form.getFieldValue('title')} />
+          </Form.Item>
+          <Form.Item name="caption">
+            <FloatLabel textarea label={t('description')} value={form.getFieldValue('caption')} />
+          </Form.Item>
+          <Form.Item name="tags">
+            <Select open={false} mode="tags" placeholder={t('tags')} />
+          </Form.Item>
+          <Form.Item label={t('allow-comments')} initialValue valuePropName="checked" name="enableComments">
+            <Switch className="switch" />
+          </Form.Item>
+          <Button block type="primary" htmlType="submit" loading={loading}>
+            {t('submit')}
+          </Button>
+        </Form>
+      </Modal>
+    </>
   )
 }
 
