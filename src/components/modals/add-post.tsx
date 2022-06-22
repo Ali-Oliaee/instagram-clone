f3ea4f88f64c741dc25bbb732305389f60726281
ali <alioliaee31@gmail.com>
2022-06-22 15:28:30 +0430

    add translations

--- src/components/modals/add-post.tsx
+++ src/components/modals/add-post.tsx
@@ -98,12 +98,7 @@ function AddPostModal() {
               <p className="ant-upload-drag-icon">
                 <InboxOutlined />
               </p>
-              <p className="ant-upload-text">Click or drag file to this area to upload</p>
-              <p className="ant-upload-hint">
-                Support for a single or bulk upload. Strictly prohibit from uploading company
-                data or other
-                band files
-              </p>
+              <p className="ant-upload-text">{t('dragger-title')}</p>
             </Dragger>
           </Form.Item>
         </Form>
