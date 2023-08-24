0778cbaba84de4728ab1aec32999cc9220a84056
ali <alioliaee31@gmail.com>
2023-08-24 13:13:18 +0330

    fix 404 page link

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -4,14 +4,13 @@ import { SwitchLanguage } from '../../components'
 import './style.scss'
 
 function NotFoundPage() {
-  const currentUser = localStorage.getItem('user')
   const { t } = useTranslation()
 
   return (
     <div className="not-found-page">
       <h1>{t('404')}</h1>
       <h3>{t('page-not-found')}</h3>
-      <Link to={!currentUser ? '/auth/login' : '/'}>{t('back-home')}</Link>
+      <Link to="/">{t('back-home')}</Link>
       <SwitchLanguage />
     </div>
   )
